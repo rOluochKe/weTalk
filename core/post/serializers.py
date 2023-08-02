3a1f78247ec3ad7172b6a915b4b00f58a2e64280
Raymond Oluoch <rasholuoch@gmail.com>
2023-08-02 13:28:41 +0300

    comments management

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -12,6 +12,10 @@ class PostSerializer(AbstractSerializer):
                                           slug_field='public_id')
     liked = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+
+    def get_comments_count(self, instance):
+        return instance.comment_set.count()
 
     def get_liked(self, instance):
 
@@ -56,6 +60,7 @@ class PostSerializer(AbstractSerializer):
             'edited',
             'liked',
             'likes_count',
+            'comments_count',
             'created',
             'updated'
             ]
